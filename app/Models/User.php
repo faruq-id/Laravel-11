786dff4d9ad55cc1643120889289988db92bbd19
Miftahul Faruq <redon.si10gta@gmail.com>
2024-11-22 15:11:41 +0700

    Penambahan filed fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,12 +19,16 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     //proteksi yang boleh di insert (create) secara massal
-    // protected $fillable = [
-    //     'name',
-    //     'usename',
-    //     'email',
-    //     'password',
-    // ];
+    protected $fillable = [
+        'name',
+        'usename',
+        'email',
+        'profile_picture',
+        'is_admin',
+        'status',
+        'last_login_at',
+        'last_login_ip',
+    ];
 
     //proteksi yang tidak boleh di insert secara massal
     protected $guarded =[
@@ -33,7 +37,9 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
-     *
+     *Properti $hidden sangat penting untuk mencegah atribut tertentu tampil dalam output model, 
+     *memberikan kontrol lebih besar atas privasi data, dan meningkatkan keamanan aplikasi.
+     *kolom ini disembunyikan agar tidak dikirimkan ke pengguna lain atau terlihat dalam output API
      * @var array<int, string>
      */
     protected $hidden = [
