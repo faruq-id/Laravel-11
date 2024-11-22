5d7c7123cce57c5de5cce2c87d91d6478abe41d4
M Agung Sutrisno <m.agung.sutrisno@gmail.com>
2024-11-22 14:32:20 +0700

    Add aceesor profile picture

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Storage;
 
 class User extends Authenticatable
 {
@@ -19,12 +20,18 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     //proteksi yang boleh di insert (create) secara massal
-    // protected $fillable = [
-    //     'name',
-    //     'usename',
-    //     'email',
-    //     'password',
-    // ];
+    protected $fillable = [
+        'name',
+        'username',
+        'phone_number',
+        'email',
+        //'password',
+        'is_admin',
+        'profile_picture',
+        'status',
+        'last_login_at',
+        'last_login_ip',
+    ];
 
     //proteksi yang tidak boleh di insert secara massal
     protected $guarded =[
@@ -57,5 +64,15 @@ class User extends Authenticatable
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class, 'author_id');
+    }
+
+    /**
+     * Digunakan untuk mengambil data profile_picture dari storage. di view tinggal manggil $user->profile_picture
+     *
+     * @return string
+     */
+    public function getProfilePictureAttribute(): string
+    {
+        return $this->profile_picture && Storage::has($this->profile_picture) ? Storage::url($this->profile_picture) : Storage::url('/users/no-image.png');
     }
 }
