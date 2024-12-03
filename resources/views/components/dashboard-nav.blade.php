b629fb94716eff05b7ec54415a8c27a17b126e28
Miftahul Faruq <redon.si10gta@gmail.com>
2024-12-04 00:11:13 +0700

    Update navbar left dashboard color icon

--- resources/views/components/dashboard-nav.blade.php
+++ resources/views/components/dashboard-nav.blade.php
@@ -39,7 +39,8 @@ x-init="$watch('openSidebar', value => localStorage.setItem('openSidebar', JSON.
 
             <li title="Mahasiswa">
                 <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-                    <svg class="w-6 h-6 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <svg class="w-6 h-6 text-gray-500 transition duration-75 
+                        dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clip-rule="evenodd"/>
                     </svg>                                            
                     <span x-show="!openSidebar" x-cloak class="ml-3">Mahasiswa</span>
@@ -142,7 +143,7 @@ x-init="$watch('openSidebar', value => localStorage.setItem('openSidebar', JSON.
                     class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg group
                     transition duration-75 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 
                     {{ request()->routeIs('admin.profile.*') || request()->routeIs('admin.users.*') ? 'bg-gray-200 dark:bg-gray-900' : '' }}">
-                    <svg class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white"
+                    <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
                     </svg>
