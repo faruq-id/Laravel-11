bb55b2c4e35f5ca5223ba6bf7527a553bf992a37
Miftahul Faruq <redon.si10gta@gmail.com>
2024-12-06 17:38:24 +0700

    Penambahan uppload images pada edit post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -42,7 +43,7 @@ class PostController extends Controller
 
     public function update(Request $request, $encryptedId)
     {
-        // dd($request);
+        //dd($request);
         try {
             // Dekripsi ID
             $id = Crypt::decrypt($encryptedId);
@@ -66,10 +67,12 @@ class PostController extends Controller
                 'status' => 'required|in:0,1', // Hanya nilai 0 atau 1 yang diperbolehkan
                 'keywords' => 'nullable|string|max:255', // Kata kunci opsional
                 'metadesc' => 'nullable|string|max:255', // Meta description opsional
+                'image' => ['sometimes', 'required', 'file', 'image', 'mimes:jpg,jpeg,png', 'mimetypes:image/jpeg,image/png', 'max:1024'],
             ]);
 
             // Temukan post berdasarkan ID
             $post = Post::findOrFail($id);
+            $oldFilePath = $post->image;
 
             // Update data pada model Post
             $post->fill($validatedData);
@@ -77,6 +80,40 @@ class PostController extends Controller
             // Cek jika tidak ada perubahan
             if ($post->isClean()) {
                 return redirect()->back()->with('info', 'Tidak ada perubahan yang disimpan.');
+            }
+
+            if ($request->hasFile('image')) {
+                // gunakan slug helper agar "nama" bisa dipakai sebagai bagian
+                // dari nama gambar_profil
+                $slug = Str::slug($request->title);
+                // Ambil extensi file asli
+                $extFile = $request->image->getClientOriginalExtension();
+                // Generate nama gambar, gabungan dari slug "nama"+time()+extensi file
+                $namaFile = $slug . '-' . time() . "." . $extFile;
+                // Proses upload, simpan ke dalam folder "uploads"
+                
+                // Simpan file baru
+                $newFilePath = $request->image->storeAs('blogs', $namaFile, 'public'); //storange/app/public/users
+                // Debugging path
+                if ($newFilePath) {
+                    // Perbarui nama file yang akan disimpan ke database
+                    $post->image = $newFilePath;
+                    // $user->save();
+            
+                    // Hapus file lama
+                    if ($oldFilePath && Storage::disk('public')->exists($oldFilePath)) {
+                        $deleted = Storage::disk('public')->delete($oldFilePath);
+            
+                        if (!$deleted) {
+                            // Jika penghapusan file lama gagal, log pesan error
+                            //Log::error("Gagal menghapus file lama: $oldFilePath");
+                            return redirect()->back()->with('error', 'Gagal menghapus file lama.');
+                        }
+                    }
+                } else {
+                    // Jika file baru gagal diunggah
+                    return redirect()->back()->with('error', 'Gagal mengunggah file baru.');
+                }  
             }
 
             // Simpan perubahan ke database
