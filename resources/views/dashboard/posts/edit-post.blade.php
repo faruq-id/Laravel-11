01ea267e53911af331a48e95079c25f32d3fc42b
Miftahul Faruq <redon.si10gta@gmail.com>
2024-12-12 17:24:40 +0700

    text area editor style tailwind

--- resources/views/dashboard/posts/edit-post.blade.php
+++ resources/views/dashboard/posts/edit-post.blade.php
@@ -125,7 +125,7 @@
                                 </div>
 
                                 {{-- TEXTAREA / TEXT EDITOR --}}
-                                <textarea id="editor" name="body" class="border border-gray-300 rounded-lg w-full p-4">{{ $post->body }}</textarea>
+                                <textarea id="editor" name="body" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">{{ $post->body }}</textarea>
                             </div>
 
                             <div class="xs:grid mb-4 h-auto max-w-4xl xl:mr-4 sm:grid-cols-2">
